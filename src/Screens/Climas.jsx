3986b3efcbb3cd5fd07ce3a652b314411ad65180
JonatanGoncalves <jonatangoncalves598@gmail.com>
2024-04-29 16:26:05 -0300

    Commit Final

--- src/Screens/Climas.jsx
+++ src/Screens/Climas.jsx
@@ -1,9 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-paper";
 
 export default function Climas() {
     return(
         <View style={styles.container}>
-            <Text>Joinville</Text>
+            <Text>Clima de Joinville - 29/04/2024</Text>
+            <Text>{"\n"}</Text>
+            <Icon 
+              source={"cloud"}
+              size={100}
+            />
+            <Text>Joinville - Nublado</Text>
+            <Icon 
+              source={"weather-pouring"}
+              size={100}
+            />
+            <Text>Araquari - Chuva</Text>
         </View>
     )
 }
